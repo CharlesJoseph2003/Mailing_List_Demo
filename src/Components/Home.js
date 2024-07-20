ec3bfa464931063f8c532d1361201b307dd61a9c
CharlesJoseph2003 <105239968+CharlesJoseph2003@users.noreply.github.com>
2024-07-20 14:38:58 -0400

    test11

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,9 +4,7 @@ import Text from './Text';
 import Email from './Email';
 import Footer from './Footer';
 import Second_Page from './Second_page';
-import Vector_12 from "../Assets/Vector 12.svg";
-import Vector_13 from "../Assets/Vector 13.svg";
-import Vector_14 from "../Assets/Vector 14.svg";
+
 // import './Home.css'; // Make sure the path is correct
 
 const Home = () => {
@@ -18,9 +16,6 @@ const Home = () => {
         <Email />
         <Footer />
         <Second_Page />
-        <img src={Vector_12} alt="Background" className="background-image-vec1" />
-        <img src={Vector_13} alt="Background" className="background-image-vec2" />
-        <img src={Vector_14} alt="Background" className="background-image-vec3" />
       </div>
     </div>
   );
